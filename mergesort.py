f0cb9f620b10207bd96b8d02299322cc055d9127
Tuni <pasi.salminen@tuni.fi>
2024-05-24 15:25:07 +0300

    recover the right version of mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -9,18 +9,12 @@ def debug_print(debug_msg=None, **kwargs):
 
 
 def mergesort(array):
-<<<<<<< HEAD
     debug_print(array=array)
-=======
->>>>>>> b05a285d7208e1df2d599b46e4d70f4e2dc93ff2
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
-<<<<<<< HEAD
     debug_print(m=m)
-=======
->>>>>>> b05a285d7208e1df2d599b46e4d70f4e2dc93ff2
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
@@ -29,11 +23,8 @@ def mergesort(array):
 
 
 def merge(left, right):
-<<<<<<< HEAD
     debug_print(debug_msg="Merging...", left=left, right=right)
 
-=======
->>>>>>> b05a285d7208e1df2d599b46e4d70f4e2dc93ff2
     merged = []
 
     while len(left) > 0 and len(right) > 0:
@@ -47,23 +38,16 @@ def merge(left, right):
     else:
         merged += right
 
-<<<<<<< HEAD
     debug_print(merged=merged)
-=======
->>>>>>> b05a285d7208e1df2d599b46e4d70f4e2dc93ff2
     return merged
 
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
-<<<<<<< HEAD
 
     input_list = input_str.split(",")
     debug_print(input_list=input_list)
 
-=======
-    input_list = input_str.split(",")
->>>>>>> b05a285d7208e1df2d599b46e4d70f4e2dc93ff2
     value_list = []
     for x in input_list:
         try:
@@ -72,10 +56,7 @@ if __name__ == "__main__":
             print("Invalid input.")
             quit(1)
 
-<<<<<<< HEAD
     debug_print(value_list=value_list)
 
-=======
->>>>>>> b05a285d7208e1df2d599b46e4d70f4e2dc93ff2
     sorted_list = mergesort(value_list)
     print(sorted_list)
